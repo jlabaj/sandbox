4893616ffd1e135c23a085ce278a343d594df43a
jLabaj <labaj.jakub@gmail.com>
2017-10-04 19:08:29 +0200

    server

--- server.js
+++ server.js
@@ -29,10 +29,6 @@ app.use('/styles', express.static(__dirname + '/views/css/'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-app.listen(port, function () {
-    
-        console.log( "Listening on server_port " + port  );
-    
-    });
+app.listen(port, ,"0.0.0.0");
 
 // app.listen(8000,"127.0.0.1");
